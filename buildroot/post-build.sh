994910724cc6bb9a9076fc19fb71f539a480bcbd
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-05-18 12:51:34 +0300

    static ip

--- buildroot/post-build.sh
+++ buildroot/post-build.sh
@@ -5,3 +5,14 @@ chmod 755 ${TARGET_DIR}/etc/init.d/S99pathfinder-service
 
 # Create symlink for SysV init in target directory
 ln -sf /etc/init.d/pathfinder-service ${TARGET_DIR}/etc/init.d/S99pathfinder-service
+
+# Modify network configuration for static IP address
+cat <<EOF > "${TARGET_DIR}/etc/network/interfaces"
+
+auto eth0
+iface eth0 inet static
+    address 192.168.1.100
+    netmask 255.255.255.0
+    gateway 192.168.1.1
+    dns-nameservers 8.8.8.8
+EOF
